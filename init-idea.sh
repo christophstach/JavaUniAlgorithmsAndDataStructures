25bf2a65ab0561fee93519a202659f834d36a8d4
Christoph Stach <christoph.stach@gmail.com>
2016-11-11 00:44:15 +0100

    added init idea script

---
+++ init-idea.sh
@@ -0,0 +1,5 @@
+cd ./exercise-01 && gradle idea && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && cd ..
+cd ./exercise-02 && gradle idea && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && cd ..
+cd ./exercise-03 && gradle idea && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && cd ..
+cd ./exercise-04 && gradle idea && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && cd ..
+cd ./exercise-05 && gradle idea && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && cd ..
