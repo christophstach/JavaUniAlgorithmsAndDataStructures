a44580abdec7f9154d1dc9dab2504deb3f78ff24
Christoph Stach <christoph.stach@gmail.com>
2016-12-14 10:45:03 +0100

    renamed package hash

--- exercise-05-hash/src/main/java/edu/christophstach/hash/Application.java
+++ exercise-05-hash/src/main/java/edu/christophstach/hash/Application.java
@@ -8,12 +8,19 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package exercise;
+package edu.christophstach.hash;
 
 /**
+ *
  * @author Christoph Stach - s0555912@htw-berlin.de
  * @since 18.10.2016
  */
-public class ApplicationTest {
-
+public class Application {
+    /**
+     * Haupt methode
+     *
+     * @param args commandline args
+     */
+    public static void main(String[] args) {
+    }
 }
