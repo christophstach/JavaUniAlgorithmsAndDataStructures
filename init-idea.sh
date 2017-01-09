8b6b3f5f10b716c00fee279f2c5b3735f5a1343c
Christoph Stach <christoph.stach@gmail.com>
2017-01-09 13:02:59 +0100

    update init-idea.sh

--- init-idea.sh
+++ init-idea.sh
@@ -4,4 +4,3 @@ cd ./exercise-03-graph-stack   && rm -f ./*.iml && gradle idea && rm -f ./*.ipr 
 cd ./exercise-04-skyline-hanoi && rm -f ./*.iml && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && rm -Rf ./.idea && cd ..
 cd ./exercise-05-hash          && rm -f ./*.iml && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && rm -Rf ./.idea && cd ..
 cd ./exercise-06-encoding      && rm -f ./*.iml && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && rm -Rf ./.idea && cd ..
-cd ./sort                      && rm -f ./*.iml && gradle idea && rm -f ./*.ipr && rm -f ./*.iws && rm -Rf ./.idea && cd ..
